0a3d2b17594734f301b7669bfecb99cd9a5db30f
shubhamm008 <156988780+shubhamm008@users.noreply.github.com>
2024-04-30 10:33:37 +0530

    Updated musicplayer.js

--- musicplayer.js
+++ musicplayer.js
@@ -1,1 +1,21 @@
 //started with music player javascript
+let musicContainer = document.querySelector(".music-container");
+let audio = document.querySelector("audio");
+let trackArt = document.querySelector(".track-art");
+let musicInfo = document.querySelector(".music-info");
+let trackName = document.querySelector(".track-name");
+let trackArtist = document.querySelector(".track-artist");
+let timelineContainer = document.querySelector(".timeline-container");
+let btnContainer = document.querySelector(".button-container");
+let shuffleBtn = document.querySelector(".shuffle-btn");
+let prevBtn = document.querySelector(".prev-btn");
+let playPauseBtn = document.querySelector(".play-pause-btn");
+let nextBtn = document.querySelector(".next-btn");
+let muteBtn = document.querySelector(".mute-btn");
+let volumeSlider = document.querySelector(".volume-slider");
+let currentTime = document.querySelector(".current-time");
+let totalTime = document.querySelector(".total-time");
+// let wave = document.getElementById("wave");
+let trackIndex = 0;
+let isPlaying = false;
+let isRandom = false;
