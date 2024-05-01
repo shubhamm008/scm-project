a8f1b32bca637c1cc7af27fbccb2d993c1b51991
shubhamm008 <156988780+shubhamm008@users.noreply.github.com>
2024-05-01 11:58:33 +0530

    Updated musicplayer.js

--- musicplayer.js
+++ musicplayer.js
@@ -26,3 +26,29 @@ const music_list=[
     artist: "sidhu moosewala",
     music: "The Last Ride - DjPunjab.Com.se.mp3", 
   },
+  {
+    img:"donwload.jpg",
+    name:"jatt da muqabala",
+    artist:"sidhu moosewala",
+    music:"Jatt_Da_Muqabla_1.mp3"
+  },
+  {
+    img:"https://images.unsplash.com/photo-1659542891330-b2969fb0741e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80",
+    name: "Faded",
+    artist: "Alan walker",
+    music:"Faded(PaglaSongs).mp3",
+  },
+  {
+    img:"https://images.unsplash.com/photo-1659425757127-ccbf96c59163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1386&q=80",
+    name:"Rather Be"
+    artist:"Clean bandit",
+    music:"Clean-Bandit-Rather-Be-feat.-Jess-Glynee.mp3",
+  },
+  {
+    img:"travis.jpg",
+    name:"butterfuly effect",
+    artist:"travis scott",
+    music:"Butterfly-Effect(PaglaWorld.Social).mp3",
+  },
+  
+  
